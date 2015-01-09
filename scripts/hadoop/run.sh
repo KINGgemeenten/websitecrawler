5eae59444f5ce970c98ce44a2b77b5986e639cf7
942u3895hjf <markus.jelsma@openindex.io>
2015-01-09 16:20:39 +0100

    use prod data

--- scripts/hadoop/run.sh
+++ scripts/hadoop/run.sh
@@ -5,33 +5,33 @@
   then
     # Check for updated inject URL's and domain URL filter
     echo Downloading updated seed.txt
-    if curl -s -f http://test:test@test-acc.triquanta.nl/seed.txt > seed.txt.new
+    if curl -s -f http://gewoontoegankelijk.nl/seed.txt > seed.txt.new
     then
       # Check it has changed
-      if ! diff -q seeds/seed.txt seed.txt.new
+      if ! diff -q /opt/nutch/seeds/seed.txt seed.txt.new
       then
         # Attempt to fetch the other file
         echo Downloading domain-urlfilter.txt
-        if curl -s -f http://test:test@test.nl/domain-urlfilter.txt > domain-urlfilter.txt.new
+        if curl -s -f http://gewoontoegankelijk.nl/domain-urlfilter.txt > domain-urlfilter.txt.new
         then
           # Check domain URL filter has changed
-          if ! diff -q conf/domain-urlfilter.txt domain-urlfilter.txt.new
+          if ! diff -q /opt/nutch/conf/domain-urlfilter.txt domain-urlfilter.txt.new
           then
             # And if all lines have at least one dot
-            if [ `grep "\n" domain-urlfilter.txt.new |  wc -l` -eq `grep . domain-urlfilter.txt.new |  wc -l` ]
-            then
+#            if [ `grep "\n" domain-urlfilter.txt.new |  wc -l` -eq `grep . domain-urlfilter.txt.new |  wc -l` ]
+#            then
               # Copy stuff to their new home
-              mv seed.txt.new inject_urls/seed.txt
-              mv domain-urlfilter.txt.new conf/domain-urlfilter.txt
+              mv seed.txt.new /opt/nutch/seeds/seed.txt
+              mv domain-urlfilter.txt.new /opt/nutch/conf/domain-urlfilter.txt
 
               # Distribute the config to the worker nodes
               distribute_config.sh
 
               # Inject some
               inject.sh
-            else
-              echo Domain-urlfilter.txt does not conform to specs
-            fi
+#            else
+#              echo Domain-urlfilter.txt does not conform to specs
+#            fi
           else
             echo Domain-urlfilter.txt has not changed
           fi
@@ -72,7 +72,7 @@
     fi
 
     # Generate a fetch list (list of URL's to download) from the CrawlDB
-    nutch generate crawl/crawldb segments/generate/ -noFilter -noNorm -numFetchers 40
+    nutch generate crawl/crawldb segments/generate/ -noFilter -noNorm -numFetchers 4
 
     # Check if we got a valid fetch list
     if [ $? -ne 0 ]
@@ -98,6 +98,9 @@
 
     # Update the CrawlDB with the fetched records and newly discovered URL's
     nutch updatedb -Dplugin.includes="urlfilter-(domain|suffix)" crawl/crawldb -dir segments/update/ -filter
+
+    # Send the stuff to the index
+    nutch index -Dmapred.reduce.tasks=12 crawl/crawldb -dir segments/update -deleteGone -noCommit
 
     # Move the finished segments to their daily finished queue
     hadoop fs -mv segments/update/* segments/finished/ &
